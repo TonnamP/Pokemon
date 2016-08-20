ad6a4a9f57978a0082affd40fe772259613025ac
thanawat <tha_chookes@hotmail.com>
2016-08-21 03:39:26 +0700

    add to tonnam repo

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 new Vue({
     el: '#app',
     data: {
-        msg: 'Pokemon Ranger'
+        msg: 'Pokemon Ranger',
+        list: []
     },
     methods:{
       getApi: function()
       {
         var url = 'http://www.pokesnipers.com/api/v1/pokemon.json'
+        var that = this
         this.$http.get(url).then(function(res){
           console.log(res.data.results);
+          this.list =  res.data.results
         })
       }
+    },
+    ready: function()
+    {
+      //alert('Pokemon')
+      this.getApi()
+      var that = this
+      // setInterval(function () { that.getApi()}, 10000)
     }
 })
 console.log(app)
